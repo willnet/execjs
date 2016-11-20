c4e5e22942d2b10dc9dee15b701b7fe10cbcd2b3
willnet <netwillnet@gmail.com>
2016-11-20 16:50:51 +0900

    more info

--- lib/execjs/external_runtime.rb
+++ lib/execjs/external_runtime.rb
@@ -209,8 +209,9 @@ module ExecJS
         end
       else
         def exec_runtime(filename)
-          Rails.logger.debug "filename: #{filename}"
-          Rails.logger.debug "binary: #{binary}"
+          Rails.logger.debug "filename: #{filename.inspect}"
+          Rails.logger.debug "binary: #{binary.inspect}"
+          Rails.logger File.read(filaname)
           io = IO.popen(binary.split(' ') << filename, @popen_options.merge({err: [:child, :out]}))
           output = io.read
           io.close
